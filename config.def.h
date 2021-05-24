aba75bf6b99ad65deca99e405d519d9e86334810
rogelio <rogelio@ssf.mx>
2021-05-24 08:08:59 -0500

    aplica parches varios

--- config.def.h
+++ config.def.h
@@ -1,20 +1,21 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int borderpx  = 4;        /* border pixel of windows */
 static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=12" };
-static const char dmenufont[]       = "monospace:size=12";
+static const int user_bh            = 25;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
+static const char *fonts[]          = { "Product Sans:size=12" };
+static const char dmenufont[]       = "Product Sans:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#ECEFF4";
 static const char col_gray4[]       = "#5E81AC";
-static const char col_cyan[]        = "#005577";
-static const char col1[]            = "#3498DB";
-static const char col2[]            = "#3498DB";
+static const char col_cyan[]        = "#88C0D0";
+static const char col1[]            = "#8FBCBB";
+static const char col2[]            = "#4C566A";
 static const char col3[]            = "#3498DB";
 static const char col4[]            = "#3498DB";
 static const char col5[]            = "#3498DB";
@@ -53,8 +54,11 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "gnome-calculator",     NULL,       NULL,       0,            1,           -1 },
+	{ "Gnome-calculator",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Pavucontrol",     NULL,       NULL,       0,            1,           -1 },
+	{ "MEGAsync",     NULL,       NULL,       0,            1,           -1 },
+
 };
 
 /* layout(s) */
@@ -84,7 +88,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-l", "7",  "-c",  "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col1, "-sf", col2, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
 static Key keys[] = {
@@ -121,11 +125,11 @@ static Key keys[] = {
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
 	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
+	TAGKEYS(                        XK_8,                      4)
+	TAGKEYS(                        XK_9,                      5)
+	TAGKEYS(                        XK_0,                      6)
+	//TAGKEYS(                        XK_8,                      7)
+	//TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
 
