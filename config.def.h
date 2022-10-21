14a5efd8053b36c68f52bf6381582826a93556b9
Rogelio Delgado <rogelio@ssf.mx>
2022-10-21 16:23:56 -0500

    Agrega fuente NotoColorEmoji para activar emojis en dwm

--- config.def.h
+++ config.def.h
@@ -7,7 +7,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 25;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const char *fonts[]          = { "Product Sans:size=12" };
+static const char *fonts[]          = { "Product Sans:size=12", "NotoColorEmoji:pixelsize=16:antialias=true:autohint=true" };
 static const char dmenufont[]       = "Product Sans:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
